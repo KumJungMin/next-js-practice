ae11ee1fe42687d35cab17ebb2e80d768991ef63
KumJungMin <rmawjdals@naver.com>
2023-11-15 15:00:11 +0900

    7. Link를 사용하면, SPA를 구현할 수 있다

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,5 +1,6 @@
 // 1. layout이 기본적인 html 구조를 잡아준다.
 
+import Link from "next/link";
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -17,13 +18,20 @@ export default function RootLayout({
 }) {
   return (
     <html>
+      {/* 
+        Link를 사용하면, SPA를 구현할 수 있음
+        - SPA란, 하나의 페이지에서 모든 작업을 처리하는 앱
+          1. 페이지 리로딩이 없어짐 
+          2. 방문했던 페이지는 다운로드 하지 않음 
+          3. 미리 페이지를 다운로드함   
+      */}
       <body>
         <h1>
-          <a href="/">WEB</a>
+          <Link href="/">WEB</Link>
         </h1>
         <ol>
           <li>
-            <a href="/read/1">html</a>
+            <Link href="/read/1">html</Link>
           </li>
           <li>
             <a href="/read/2">css</a>
@@ -32,10 +40,10 @@ export default function RootLayout({
         {children}
         <ul>
           <li>
-            <a href="/create">create</a>
+            <Link href="/create">create</Link>
           </li>
           <li>
-            <a href="/update/id">update</a>
+            <Link href="/update/id">update</Link>
           </li>
           <li>
             <button>delete</button>
