6c51e0d6e40bdeaf426dddd9b301f68a063485bb
KumJungMin <rmawjdals@naver.com>
2023-11-15 15:27:20 +0900

    12. 서버 컴포넌트는 사용자 상호작용이 없을 때 유용

--- my-app/src/app/read/[id]/page.tsx
+++ my-app/src/app/read/[id]/page.tsx
@@ -1,10 +1,14 @@
 // 동적 라우터는 폴더명을 []로 감싸줘야 한다.
+// 데이터를 보여주기만 하므로, 서버 컴포넌트여야 함
 
-export default function Read({ params }: { params: { id: string } }) {
+export default async function Read({ params }: { params: { id: string } }) {
+  const id = params.id;
+  const resp = await fetch(`http://localhost:9999/topics/${id}`);
+  const topic = await resp.json();
   return (
     <>
-      <h2>Read Page</h2>
-      {params.id}
+      <h2>{topic.title}</h2>
+      {topic.body}
     </>
   );
 }
