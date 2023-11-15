f99f4f9726c3e356721f1204ec26f4d0c8fbf957
KumJungMin <rmawjdals@naver.com>
2023-11-15 15:05:39 +0900

    8. Image 태그로 이미지 등록하는 법

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,3 +1,18 @@
+import Image from "next/image";
+
+// nextJS에서는 이미지를 사용할 때, Image 컴포넌트를 사용
+// https://nextjs.org/docs/pages/api-reference/components/image
 export default function Home() {
-  return <>Hello</>;
+  return (
+    <>
+      <h2>Welcome</h2>
+      {/* public 폴더에 있는 이미지는 경로를 '/' 형식으로 시작하면 됨 */}
+      <Image
+        src="/next.svg"
+        width={100}
+        height={50}
+        alt="nextJS logo"
+      />
+    </>
+  );
 }
